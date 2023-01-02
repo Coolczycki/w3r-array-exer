4f9c2720ad0186dca56e32a8169a8d59eba02fcb
Piotr_Kulczycki <Piotr_kulczycki@icloud.com>
2023-01-02 13:49:21 +0100

    Setup for exercise 07

---
+++ 07.js
@@ -0,0 +1,12 @@
+/* 
+07. Write a JavaScript program to sort the items of an array. 
+Sample array : var arr1 = [ 3, 8, 7, 6, 5, -4, 3, 2, 1 ];
+Sample Output : -4,-3,1,2,3,5,6,7,8 
+*/
+
+const sorted = function sortedArr(arr) {
+  return arr.sort();
+}
+
+console.log(sorted([-6, -3, 8, 7, 6, 5, -4, 3, 2, 1 ]));
+
